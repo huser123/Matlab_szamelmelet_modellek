3bc464b9d41c72a09072713d94019d1dd9ca1434
huse123 <regcim1234@gmail.com>
2020-12-25 12:32:07 +0100

    master

---
+++ Matlab_aszimp_tetszoleges_hatvany.m
@@ -0,0 +1,21 @@
+% A tetszoleges hatvany aszimptotikus surusegenek abrazolasa
+
+clear, clc
+
+an=0
+n=0
+i=0
+
+
+for n = 1:10000
+    if mod(nthroot(n,3),1) == 0
+        i=i+1
+        an=an+1
+        oszto(i)=n
+        szamitas(i)=an
+    end
+    
+    hold on
+    plot(n,(an/n),'r.')
+    %drawnow
+end
